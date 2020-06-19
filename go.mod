76859add08ed5975fdc4aa86786a888b0bc49623
Lenka <lenocka369@gmail.com>
2020-06-19 17:45:28 +0200

    Add logger - sirupsen/logrus

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/goat-project/goat-os
 
 go 1.13
+
+require (
+	github.com/sirupsen/logrus v1.2.0
+	github.com/spf13/viper v1.7.0
+)
---
+++ logger/logger.go
@@ -0,0 +1,62 @@
+package logger
+
+import (
+	"os"
+
+	"github.com/goat-project/goat-os/constants"
+	"github.com/spf13/viper"
+
+	"github.com/sirupsen/logrus"
+)
+
+// Init initializes logrus by configuration.
+func Init() {
+	path := viper.GetString(constants.CfgLogPath)
+	switch path {
+	case "":
+		if viper.GetBool(constants.CfgDebug) {
+			InitLogToStdoutDebug()
+		} else {
+			InitLogToStdout()
+		}
+	default:
+		if viper.GetBool(constants.CfgDebug) {
+			InitLogToFileDebug(path)
+		} else {
+			InitLogToFile(path)
+		}
+	}
+}
+
+// InitLogToStdoutDebug inits logrus to log the debug severity or above to Stdout.
+func InitLogToStdoutDebug() {
+	logrus.SetFormatter(&logrus.TextFormatter{
+		ForceColors: true,
+	})
+	logrus.SetOutput(os.Stdout)
+	logrus.SetLevel(logrus.DebugLevel)
+}
+
+// InitLogToStdout inits logrus to log the info severity or above to Stdout.
+func InitLogToStdout() {
+	logrus.SetFormatter(&logrus.TextFormatter{})
+	logrus.SetOutput(os.Stdout)
+}
+
+// InitLogToFile inits logrus to log the info severity or above to the file.
+func InitLogToFile(logPath string) {
+	logrus.SetFormatter(&logrus.TextFormatter{})
+
+	f, err := os.OpenFile(logPath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0600)
+	if err != nil {
+		logrus.Fatalf("error opening file: %v", err)
+	}
+
+	logrus.SetOutput(f)
+}
+
+// InitLogToFileDebug inits logrus to log the debug severity or above to the file.
+func InitLogToFileDebug(logPath string) {
+	InitLogToFile(logPath)
+	logrus.SetLevel(logrus.DebugLevel)
+}
