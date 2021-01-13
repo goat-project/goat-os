d8115965c24a4ac59e287988fc0424d8259a369d
Lenka Svetlovska <lenocka369@gmail.com>
2021-01-13 16:38:31 +0100

    Add client to run accounting

---
+++ client/client.go
@@ -0,0 +1,39 @@
+package client
+
+import (
+	"sync"
+
+	"github.com/goat-project/goat-os/resource"
+
+	"github.com/goat-project/goat-os/filter"
+	"github.com/goat-project/goat-os/preparer"
+	"github.com/goat-project/goat-os/processor"
+)
+
+// Client runs application.
+type Client struct {
+}
+
+// Run reads, filters and writes Accountable.
+func (c *Client) Run(processor processor.Interface, filter filter.Interface, preparer preparer.Interface) {
+	var mapWg sync.WaitGroup
+	//mapWg.Add(1)
+
+	//go preparer.InitializeMaps(&mapWg)
+
+	// initialize channels
+	read := make(chan resource.Resource)
+	filtered := make(chan resource.Resource)
+	fullInfo := make(chan resource.Resource)
+
+	// create done channel
+	done := make(chan bool)
+	defer close(done)
+
+	go processor.ListResources(read)
+	go filter.Filter(read, filtered)
+	go processor.RetrieveInfoResource(filtered, fullInfo)
+	go preparer.Prepare(fullInfo, done, &mapWg)
+
+	<-done
+}
