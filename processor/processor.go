6e04463cbdfbdf3bdfc2155b35c946a2c5251201
Lenka Svetlovska <lenocka369@gmail.com>
2021-01-13 16:36:23 +0100

    Add processor interface

---
+++ processor/processor.go
@@ -0,0 +1,58 @@
+package processor
+
+import (
+	"sync"
+
+	"github.com/goat-project/goat-os/resource"
+	"github.com/remeh/sizedwaitgroup"
+
+	log "github.com/sirupsen/logrus"
+)
+
+// Processor to process resource data.
+type Processor struct {
+	proc processorI
+}
+
+type processorI interface {
+	Process(chan resource.Resource, *sizedwaitgroup.SizedWaitGroup)
+	RetrieveInfo(chan resource.Resource, *sync.WaitGroup, resource.Resource)
+}
+
+const wgSize = 10
+
+// CreateProcessor creates Processor to manage reading from OpenNebula.
+func CreateProcessor(proc processorI) *Processor {
+	return &Processor{
+		proc: proc,
+	}
+}
+
+// ListResources calls method to list resource from OpenNebula.
+func (p *Processor) ListResources(read chan resource.Resource) { // todo remove sized waitgroup
+	swg := sizedwaitgroup.New(wgSize + 1)
+
+	swg.Add()
+	go p.proc.Process(read, &swg)
+
+	swg.Wait()
+	close(read)
+}
+
+// RetrieveInfoResource range over filtered resource and calls method to retrieve resource info.
+func (p *Processor) RetrieveInfoResource(filtered, fullInfo chan resource.Resource) {
+	var wg sync.WaitGroup
+
+	for accountable := range filtered {
+		if accountable == nil {
+			log.WithFields(log.Fields{"error": "no accountable"}).Error("error retrieve resource info")
+			continue
+		}
+
+		wg.Add(1)
+		go p.proc.RetrieveInfo(fullInfo, &wg, accountable)
+	}
+
+	wg.Wait()
+	close(fullInfo)
+}
