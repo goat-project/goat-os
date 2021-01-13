3ecd7a2c646f51c5b3491058d2ad73759e727c5f
Lenka Svetlovska <lenocka369@gmail.com>
2021-01-13 16:37:18 +0100

    Add error constants

---
+++ constants/error.go
@@ -0,0 +1,30 @@
+package constants
+
+// constants with error messages
+const (
+	ErrCreatePrepReaderNil  = "error create Preparer when reader is nil"
+	ErrCreatePrepLimiterNil = "error create Preparer when limiter is nil"
+	ErrCreatePrepConnNil    = "error create Preparer when gRPC client connection is nil"
+
+	ErrPrepEmptyNetUser = "error prepare empty NetUser"
+	ErrPrepNoNetUser    = "error get id, unable to prepare network record"
+
+	ErrPrepIPv4 = "unable to prepare ipv4 network record"
+	ErrPrepIPv6 = "unable to prepare ipv6 network record"
+
+	ErrPrepEmptyImage = "error prepare empty Image"
+	ErrPrepNoImage    = "error get id, unable to prepare storage record"
+
+	ErrPrepRegTime = "error get REGTIME, unable to prepare record"
+	ErrPrepSize    = "error get SIZE, unable to prepare record"
+
+	ErrPrepEmptyVM = "error prepare empty Virtual machine"
+
+	ErrPrepWrite = "error send record"
+
+	ErrNoSiteName  = "no site name in configuration"
+	ErrNoCloudType = "no cloud type in configuration"
+	ErrNoGroupName = "no group name"
+
+	ErrCreateProcReaderNil = "error create Processor when Reader is nil"
+)
